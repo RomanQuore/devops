2ff25efe0d419aab8c2c887ae66d8dafea76f7a8
Daniel_roman001 <139508526+RomanQuore@users.noreply.github.com>
2024-04-29 17:26:31 -0500

    Add files via upload

---
+++ src/lb_deploydocker.groovy
@@ -0,0 +1,11 @@
+package org.devops
+
+def desplieguecontenedor(projectGitName){
+    //sh "docker stop ${projectGitName}"
+    //sh "docker rm ${projectGitName}"
+    sh "docker pull josecorreav/react-test-jenkinsfile"
+    sh """" docker run -d --name ${projectGitName} \
+    --network=${env.NameNetwork} -p 5174:5174 \
+    --user root josecorreav/${projectGitName}
+    """
+}
