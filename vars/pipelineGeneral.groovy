956d72b555ad9f1085dcd9393fb1bf5c9ad842b8
Daniel_roman001 <139508526+RomanQuore@users.noreply.github.com>
2024-04-29 17:58:51 -0500

    Add files via upload

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,48 +1,64 @@
 def call(){
 
-    pipeline {
+    pipeline{
 
         agent any
-        tools {
-            nodejs 'NodeJS18'
+
+        tools{
+            nodejs 'NodeJS'
         }
-        /*triggers {
-          pollSCM('* * * * *') // Programa la verificación del repositorio cada minuto
-        }*/
-        environment {
-           PROJECT = "${env.UrlGitHub}".replaceAll('.+/(.+)\\.git', '$1').toLowerCase()
+        
+        environment{
+            projectName = "${env.UrlGitHub}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
         } 
-        stages {
-      
-             stage('Fase 2: Construccion de imagen en Docker Desktop') {
+
+       /* triggers{
+            pollSCM('* * * * * 1-5')
+        } */
+        
+
+        stages{
+
+            stage('Fase 2: Construcción de imagen en Docker Desktop') {
                 steps {
                     script {
                         def buildimage = new org.devops.lb_buildimagen()
-                        buildimage.buildImageDocker("${PROJECT}")
+                        buildimage.buildImageDocker("${projectName}")
                     }
                 }
-                
             }
 
-            stage('Fase 2: Publicar imagen a Docker Hub') {
+            stage('Fase 2: publicar imagen a docker hub.') {
                 steps {
                     script {
                         def publicImage = new org.devops.lb_publicardockerhub()
-                        publicImage.publicarimage("${PROJECT}")
+                        publicImage.publicarImage("${projectName}")
                     }
-                }
+                    
+                } 
             }
 
-            stage('Fase 2: Desplegar imagen en Docker') {
+            stage('Fase 2: Desplegar imagen en docker') {
                 steps {
-                    script {
-                        def deployImg = new org.devops.lb_deploydocker()
-                        deployImg.despliegueContenedor("${PROJECT}")
+                    script{
+                            def deployImg = new org.devops.lb_deploydocker()
+                            deployImg.despliegueContenedor("${projectName}")
+                        }
+                    }    
+                }                                        
+
+           stage('Fase 2: analisis con owasp') {
+                steps {
+                    script{
+                        def owasp = new org.devops.lb_owasp()
+                        owasp.AnalisisOwasp("${projectName}")
                     }
                 }
-            }
-                   
+
         }
-        
+    
+
     }
+    
+  }
 }
