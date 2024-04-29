6e3a3eec3b71a82c0c8c3ac06ac82292486e0bcf
Daniel_roman001 <139508526+RomanQuore@users.noreply.github.com>
2024-04-29 17:48:09 -0500

    Update lb_deploydocker.groovy

--- src/lb_deploydocker.groovy
+++ src/lb_deploydocker.groovy
@@ -6,6 +6,6 @@ def desplieguecontenedor(projectGitName){
     sh "docker pull josecorreav/react-test-jenkinsfile"
     sh """" docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
-    --user root josecorreav/${projectGitName}
+    --user root RomanQuore/${projectGitName}
     """
 }
